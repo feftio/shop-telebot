848cedec0eb65cf1617520bc2b0957d45bd13103
Eduard Lik <feft99@gmail.com>
2021-08-21 05:00:58 +0600

    Create Event class in utils

--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
 from __future__ import annotations
 import typing as t
+import json
 
+class Event:
+    NAME_ALIAS: str = 'n'
+    DATA_ALIAS: str = 'd'
+    __slots__ = ('name', 'data')
+
+    @classmethod
+    def loads(cls, dumped: str) -> Event:
+        event, loads = Event(), json.loads(dumped)
+        event.name, event.data = loads[cls.NAME_ALIAS], loads[cls.DATA_ALIAS]
+        return event
+
+    @classmethod
+    def dumps(cls, name: str, data: str) -> str:
+        return json.dumps({cls.NAME_ALIAS: name, cls.DATA_ALIAS: data})
 
 def _(name: str, format: str = 'html', join: bool = True) -> t.Union[str, list]:
     with open(f'{name}.{format}', 'r', encoding='utf-8') as f:
