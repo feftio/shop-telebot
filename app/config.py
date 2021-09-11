d88c51c4aa6e8af6ca21bb8e325f680adc9cc5de
Eduard Lik <feft99@gmail.com>
2021-09-11 16:18:36 +0600

    Make parsing products folder to PRODUCTS, refactoring

--- app/config.py
+++ app/config.py
@@ -1,27 +1,27 @@
-from utils import view_callback
-from decouple import config
-from os import path, listdir
+from utils import content_callback, dictify
+from dotenv import dotenv_values
+from os import path
 import sys
 
 ROOT_DIR = path.abspath(path.join(path.dirname(__file__), '..'))
-VIEWS_DIR = path.join(ROOT_DIR, config('VIEWS_DIR', 'views'))
-PRODUCTS_DIR = path.join(VIEWS_DIR, "products")
 sys.path.append(ROOT_DIR)
-view = view_callback(VIEWS_DIR)
-
-onlyfiles = listdir(PRODUCTS_DIR)
-print(onlyfiles)
+env = {**dotenv_values("test.env")}
 
 
-# TOKEN = config('TOKEN', '').strip()
-# ADMINS = config('ADMINS', '').strip()
-# PRODUCTS = {}
-# VIEWS = {
-#     'START': view('start'),
-#     'START_ADMIN': view('start_admin'),
-#     'MENU': view('menu'),
-#     'SENT_ADMIN': view('sent_admin'),
-#     'SENT': view('sent'),
-#     'NO_PRODUCT': view('no_product'),
-#     'NO_COMMAND': view('no_command')
-# }
+VIEWS_DIR = path.join(ROOT_DIR, env.get('VIEWS_DIR', 'views'))
+PRODUCTS_DIR = path.join(VIEWS_DIR, "products")
+
+view = content_callback(VIEWS_DIR)
+
+TOKEN = env.get('TOKEN', '').strip()
+ADMINS = env.get('ADMINS', '').strip()
+PRODUCTS = dictify(PRODUCTS_DIR)
+VIEWS = {
+    'START': view('start'),
+    'START_ADMIN': view('start_admin'),
+    'MENU': view('menu'),
+    'SENT_ADMIN': view('sent_admin'),
+    'SENT': view('sent'),
+    'NO_PRODUCT': view('no_product'),
+    'NO_COMMAND': view('no_command')
+}
